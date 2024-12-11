83744127d83d6defe27f301749eb1c1c3102433c
vichy <162613526+thundervivi@users.noreply.github.com>
2024-12-11 07:04:39 -0800

    Get a random choice for the computer
    I add those lines of codes to get a random choice and return a choice for the computer

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3); 
+    if (randomNumber === 0){
+        return "Rock"
+    } else if (randomNumber ===1){
+        return "Paper";
+    } else{
+        return "Scissors"
+    }
+}
+getComputerChoice();
